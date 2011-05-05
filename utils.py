f692db9dc73baf2ceceb17e5debe04dda8d6574f
Yuval Adam <yuv.adm@gmail.com>
2011-05-06 01:11:27 +0300

    added AS(adj) function

--- utils.py
+++ utils.py
@@ -3,8 +3,18 @@ from consts import SN, SN_
 
 def NS(noun):
     """Noun sexiness function
-    Returns a real value measure of the maximum similarity a noun `n not in SN`
-    has to each of the `nouns in SN_`
+    Returns a real value measure of the maximum similarity
+    a `noun not in SN` has to each `noun in SN_`
     """
     # return 10**-7 for nouns that occur less than 200 times
     return brown_collection.tf_idf(noun, SN_)
+
+def AS(adj):
+    """Adjective sexiness function
+    Returns a real value measure of how likely an adjective `adj`
+    is to modify a `noun in SN`.
+    """
+    # implementation should return relative frequency of adf
+    # in sentences in erotica corpus that contain at least
+    # one noun in SN
+    return 0
