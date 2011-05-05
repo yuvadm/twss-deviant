81abdcb8d014bedd8c9668ef637afae561dd9b1c
Yuval Adam <yuv.adm@gmail.com>
2011-05-06 01:14:43 +0300

    some docstring updates

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from consts import SN, SN_
 def NS(noun):
     """Noun sexiness function
     Returns a real value measure of the maximum similarity
-    a `noun not in SN` has to each `noun in SN_`
+    a `noun` not in SN has to each noun in SN_
     """
     # return 10**-7 for nouns that occur less than 200 times
     return brown_collection.tf_idf(noun, SN_)
@@ -12,9 +12,17 @@ def NS(noun):
 def AS(adj):
     """Adjective sexiness function
     Returns a real value measure of how likely an adjective `adj`
-    is to modify a `noun in SN`.
+    is to modify a noun in SN
     """
-    # implementation should return relative frequency of adf
+    # implementation should return relative frequency of adj
     # in sentences in erotica corpus that contain at least
     # one noun in SN
+    return 0
+
+def VS(verb):
+    """Verb sexines function
+    Returns a real value measure of how much more likely a verb 
+    phrase `verb` is to appear in an erotic context than a
+    nonerotic one
+    """
     return 0
