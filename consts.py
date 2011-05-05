90246e005f04310c65c84fcf3ac0f2aac3f498d3
Yuval Adam <yuv.adm@gmail.com>
2011-05-06 00:13:43 +0300

    added constants and started utils

---
+++ consts.py
@@ -0,0 +1,5 @@
+# set of sexually explicit nouns
+SN = ['ass', 'boobs', 'cock', 'crotch', 'cunt', 'dick']
+
+# set of sexually explicit nouns that are likely targets for euphemism
+SN_ = SN
---
+++ utils.py
@@ -0,0 +1,12 @@
+from consts import SN, SN_
+
+def NS(n):
+    """Noun sexiness function
+    Returns a real value measure of the maximum similarity a noun `n not in SN`
+    has to each of the `nouns in SN_`
+    """
+    for noun in SN_:
+        acv = ()
+        # let acv be the vector of absolute frequencies of each adjective that
+        # modifies the noun in the union of the erotica and Brown corpora
+    return 0
