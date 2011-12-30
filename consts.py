23ec3d179380212a06fed2e75d57fa5d768d5176
Yuval Adam <yuv.adm@gmail.com>
2011-12-30 13:16:56 +0100

    added BP set of body parts

--- consts.py
+++ consts.py
@@ -3,3 +3,6 @@ SN = ['ass', 'boobs', 'cock', 'crotch', 'cunt', 'dick']
 
 # set of sexually explicit nouns that are likely targets for euphemism
 SN_ = SN
+
+# set of body part nouns
+BP = ['hair', 'head', 'neck', 'eyes', 'nose', 'tounge', 'teeth', 'chin', 'chest']
